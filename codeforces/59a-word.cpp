cc4b95ca1c153bfa3f64a9dcd2d451c5f78f03ad
Abdullah Al Mahmud <aamahmud.cs@gmail.com>
2024-01-09 01:39:58 +0600

    first commit

---
+++ codeforces/59a-word.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    getline(cin, s);
+ 
+    int uppercount = 0, lowercount = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (isupper(s[i]))
+            uppercount++;
+        else
+            lowercount++;
+    }
+ 
+    if (uppercount > lowercount)
+    {
+        for (int i = 0; i < s.length(); i++)
+            putchar(toupper(s[i]));
+    }
+    else
+    {
+        for (int i = 0; i < s.length(); i++)
+            putchar(tolower(s[i]));
+    }
+ 
+    return 0;
+}
